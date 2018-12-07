f88d28da7bcc6e030771657a95806fd3236eebc3
furuiyang <ruiyang0715@gmail.com>
2018-12-07 17:52:55 +0800

    加注释

--- sync2.py
+++ sync2.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 class SyncData:
     def __init__(self):
         self.mysql_host = "139.159.176.118"
-        self.mysql_username = "dcr"
+        self.mysql_username = "ruiyang"
         self.mysql_password = "xxxxxxxxx"
         self.mysql_DBname = "datacenter"
         self.mysql_port = 3306
@@ -33,23 +33,29 @@ class SyncData:
         return col
 
     @staticmethod
-    def generate_sql_table_column_names(connection, db_name, table_name):
-
+    def generate_sql_head_name_list(connection, db_name, table_name):
+        """
+        生成mysql中一个table中所有字段组成的列表
+        :param connection:
+        :param db_name:
+        :param table_name:
+        :return:
+        """
         query_sql = """
         select COLUMN_NAME, DATA_TYPE, column_comment from information_schema.COLUMNS 
         where table_name="{}" and table_schema="{}";
         """.format(table_name, db_name)
 
-        sql_table_col_name_list = list()
+        head_name_list = list()
         try:
             with connection.cursor() as cursor:
                 cursor.execute(query_sql)
                 res = cursor.fetchall()
                 for i in res:
-                    sql_table_col_name_list.append(i[0])
+                    head_name_list.append(i[0])
         finally:
             connection.commit()
-        return sql_table_col_name_list
+        return head_name_list
 
     def generate_sql_table_length(self, connection, table_name):
         """
@@ -71,15 +77,22 @@ class SyncData:
         return table_length
 
     def generate_mongo_collection_length(self, mongo_collection):
+        """
+        查询集合中文档总数
+        :param mongo_collection:
+        :return:
+        """
         return mongo_collection.find().count()
 
     def generate_sql_table_datas_list(self, connection, table_name, name_list):
         try:
             with connection.cursor() as cursor:
-                num = 1000
+                # 遍历生成器的时候 每次返回10000条待插入数据
+                # 原因：如果不适用 generate 几百万条数据 容易爆内存...
+                num = 10000
                 start = 0
                 while True:
-                    # 坑... 不能使用id作为偏移标准 有的表的id是不连续唯一的 会造成多插数据
+                    # 注意： 不能使用id作为偏移标准 在mysql表id不连续的情况下 会造成多插数据
                     # query_sql = """
                     # select * from {} where id > {} limit {};""".format(table_name, start, num)
                     # eg. 0, 10000
@@ -88,15 +101,14 @@ class SyncData:
                     select * from {} limit {},{};""".format(table_name, start, num)
 
                     cursor.execute(query_sql)
+                    # res 是每次从mysql取出的10000条（或者最后一次少于10000条）的数据
                     res = cursor.fetchall()
                     if not res:
                         break
                     start += num
 
                     # 生成器部分
-                    # 改进：之前是生成器每次 yield 出一个字典 使用 insert 插入
-                    #      现在是每次 yield 出一个列表 使用 insert_many 插入
-                    #      just try try try ...
+                    # 每次 yield 出一个字典嵌套列表 使用 insert_many 插入
                     yield_column_list = list()
                     for column in res:
                         column_dict = self.zip_doc_dict(name_list, column)
@@ -107,6 +119,12 @@ class SyncData:
 
     @staticmethod
     def zip_doc_dict(name_list, column_tuple):
+        """
+        将mysql字段和每一行数据组成一个字典 便于mongodb插入
+        :param name_list:
+        :param column_tuple:
+        :return:
+        """
         assert len(name_list) == len(column_tuple)
         name_tuple = tuple(name_list)
         column_dict = dict(zip(name_tuple, column_tuple))
@@ -124,16 +142,16 @@ class SyncData:
 
     def write_datas2mongo(self, mongo_collection, sql_table_datas_list):
         try:
-            for each_sql_table_data in sql_table_datas_list:
-                # each_sql_table_data 是一个 yield 出的 list
-                # j_list 是组装好的需要重新进行插入的 list
+            for yield_list in sql_table_datas_list:
+                # yield_list 是一个生成器 yield 出的 list
+                # j_list 是通过数据类型转换可进行插入的 list
                 j_list = list()
-                for j in each_sql_table_data:
-                    # j 是每一个 需要进行check 的 dict
+                for j in yield_list:
+                    # j 是每一个 需要进行数值 check 的 dict
                     j = self.check_each_sql_table_data(j)
                     j_list.append(j)
                 res = mongo_collection.insert_many(j_list)
-                print("insert ----ss---- success", res)
+                print("--- insert many success --- ", res)
         except Exception as e:
             print(e)
 
@@ -150,48 +168,33 @@ class SyncData:
                     sql_table_name_list.append(column[0])
         finally:
             connection.commit()
-
         return sql_table_name_list
 
     def sync_data(self):
+        """
+        同步数据
+        :return:
+        """
+        # 创建 mysql 连接对象 conn
         conn = self.generate_mysqlconnection()
+        # 拿到mysql数据库中的所有table的列表
         sql_table_name_list = self.gen_sql_table_name_list(conn)
 
-        '''
-        sql_table_name_list = ['bas_induinfo', 'comcn_balancesheet', 'comcn_cashflowsheet',
-        'comcn_equitychangessheet', 'comcn_incomesheet', 'comcn_induinfo', 'comcn_qcashflowsheet',
-        'comcn_qincomesheet', 'comcn_reservereportdate', 'comcn_sharesholder',
-        'comcn_sharesstructurechange', 'cominv_interactive', 'const_companynamechange',
-        'const_sector', 'const_secumain', 'const_specialnotice', 'const_stockabbrchange',
-        'const_tradingday', 'hkland_historycashflow', 'hkland_historytradestat', 'hkland_shares',
-        'index_baseinfo', 'index_baseinfosup', 'index_quot_day', 'index_weight',
-        'stk_dividendinfo', 'stk_quotidx_day', 'stk_quotidxwind_day', 'stk_quotori_day']
-        '''
-
-        # 谨慎起见 trans one by one
-        # sql_table_name_list = ['bas_induinfo']  # √ 484
-        # sql_table_name_list = ['comcn_induinfo']  # √ 5643 1.40min
-        # sql_table_name_list = ['stk_dividendinfo']  # √ 31925 8.22min
-        # sql_table_name_list = ['index_baseinfosup']  # √ 683 0.37min
-        # sql_table_name_list = ['index_baseinfo']  # √ 9096 6.27min
-        # sql_table_name_list = ['hkland_historytradestat']  # √
-        sql_table_name_list = ['index_quot_day']  # √
-
+        # 遍历每一个table_name, 实现mysql中一个table--->mongodb中的一个collection的映射
         for table_name in sql_table_name_list:
-            print("table_name is ------> ", table_name)
-            table_name_list = self.generate_sql_table_column_names(conn, self.mysql_DBname,
-                                                                   table_name)
-            print("table_name_list is ======> ", table_name_list)
+            # 生成mysql table 的字段列表，它们作为之后生成的每一个 mongodb doc 的键
+            head_name_list = self.generate_sql_head_name_list(conn, self.mysql_DBname, table_name)
 
             # type(sql_table_datas_list) --> generator
+            # 一个每次返回部分待插入数据的生成器 在写入mongodb时遍历这个生成器
             sql_table_datas_list = self.generate_sql_table_datas_list(conn, table_name,
-                                                                      table_name_list)
-
+                                                                      head_name_list)
+            # 创建一个mongodb数据库的连接对象
             mongo_collection = self.generate_mongo_collection(self.mongo_DBname, table_name)
-
+            # 向mongodb中插入数据
             self.write_datas2mongo(mongo_collection, sql_table_datas_list)
 
-            # 校对数量一致
+            # 校对两种数据库前后数量一致
             mongo_collection_length = self.generate_mongo_collection_length(mongo_collection)
             print("The generete mongodb collection length is: ", mongo_collection_length)
             sql_table_length = self.generate_sql_table_length(conn, table_name)[0]
@@ -202,6 +205,7 @@ class SyncData:
         """
         考虑两种情况：
         (1) mysql 每一个 table 中的一行数据为最小变更 可以根据集合进行增删 update
+        (2) 以字段为最小变更。需要拿到详细的一份更新日志
         :return:
         """
         pass
